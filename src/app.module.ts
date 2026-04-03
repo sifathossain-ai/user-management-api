07a0e05da1fc3005730b97550e63d1091e034f73
Sifat Hossain <sifathossain1326@gmail.com>
2026-04-03 13:05:52 +0600

    Successfully Connect Database

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: 5432,
+      username: process.env.DB_USER,
+      password: '@sifat143#',
+      database: process.env.DB_NAME,
+      entities: [],
+      synchronize: true,
     }),
   ],
   controllers: [AppController],
