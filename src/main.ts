7498387b8b88bc36bb621ed8bd984d4c5aab95aa
Sifat Hossain <sifathossain1326@gmail.com>
2026-04-03 12:53:12 +0600

    Add Validation Pipes in Globally

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
+/* eslint-disable @typescript-eslint/no-floating-promises */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+    }),
+  );
   await app.listen(process.env.PORT ?? 3002);
 }
 bootstrap();
